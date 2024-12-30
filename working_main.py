83541c2ec10525c30c173d720fa69a819e179d44
cygnusxiscool123 <ypan27@g.ucla.edu>
2024-12-29 17:31:09 -0800

    Update working_main.py
    pin_manager class

--- working_main.py
+++ working_main.py
@@ -3,21 +3,15 @@ import busio
 import microcontroller
 import time
 import digitalio
-from ADS1118 import ADS1118, ADS1118_MUX_SELECT, ADS1118_FSR, ADS1118_SAMPLE_RATE
-from spi import spi_bus, spi_device
+import pin_manager
+from drivers.ads1118 import ADS1118, ADS1118_MUX_SELECT, ADS1118_FSR, ADS1118_SAMPLE_RATE
+ 
+cs_pins = [microcontroller.pin.PA18, microcontroller.pin.PA23, microcontroller.pin.PA22, microcontroller.pin.PB23, microcontroller.pin.PB22, microcontroller.pin.PA27] 
 
-spi = spi_bus(clock=microcontroller.pin.PA17, MOSI=board.MOSI, MISO=board.MISO)
- 
-cs_pins = [board.ADC_CS1, board.ADC_CS3, board.ADC_CS2, board.ADC_CS4, board.ADC_CS5, board.ADC_CS6]
- 
-spi_devices = []
 adcs = []
  
 for cs_pin_number in cs_pins:
-    cs_pin = digitalio.DigitalInOut(cs_pin_number)
-    spi_dev = spi_device(spi, cs_pin)
-    adc = ADS1118(spi_dev)
-    spi_devices.append(spi_dev)
+    adc = ADS1118(clock=microcontroller.pin.PA17, MOSI=microcontroller.pin.PA16, MISO=microcontroller.pin.PA19, ss=cs_pin_number)
     adcs.append(adc)
     
 async def main():
@@ -26,16 +20,13 @@ async def main():
         channels = []
         for adc in adcs:
             for channel in range(4, 8):  
-            # Read from single-ended channels 4 to 7 measuring voltage relative to ground
                 tasks.append(adc.take_sample(channel))
                 channels.append(channel)
-            
-            # Read from differential channels 0 to 3 measuring voltage difference between two input pins
+                
             for channel in range(0, 4):  
                 tasks.append(adc.take_sample(channel))
                 channels.append(channel)
-            
-            # Read temperature
+                
             tasks.append(adc.take_sample(ADS1118_MUX_SELECT.TEMPERATURE))
             channels.append(ADS1118_MUX_SELECT.TEMPERATURE)
  
@@ -48,7 +39,6 @@ async def main():
                 print(f"Sample Value {i}: {result:.4f} V")
  
         await asyncio.sleep(1)   
-    
     
 if __name__ == "__main__":
     asyncio.run(main())
